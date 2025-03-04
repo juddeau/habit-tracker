06333164a9fb740c46e20e5ca09a90122149a628
plag1atv <tkayumov06@gmail.com>
2025-03-04 20:03:27 +0300

    работает

--- back/main.py
+++ back/main.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///klient.db'
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300), nullable=False)
+    password= db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
@@ -16,5 +25,15 @@ def donat():
     return render_template("donat.html")
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("registration.html")
+
+@app.route('/login')
+def login():
+    return render_template("entrance.html")
+
+
 if __name__ == '__main__':
+
     app.run(debug=True)
